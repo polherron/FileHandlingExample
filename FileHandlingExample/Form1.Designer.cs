cfe735818110358c66c38026c5bae33f2dbd4fc1
polherron <pol.herron@gmail.com>
2019-05-09 11:13:38 +0100

    Added functionality

--- FileHandlingExample/Form1.Designer.cs
+++ FileHandlingExample/Form1.Designer.cs
@@ -28,13 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnWrite = new System.Windows.Forms.Button();
+            this.btnRread = new System.Windows.Forms.Button();
+            this.btnAppend = new System.Windows.Forms.Button();
+            this.rtbWrite = new System.Windows.Forms.RichTextBox();
+            this.rtbRead = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnWrite
+            // 
+            this.btnWrite.Location = new System.Drawing.Point(64, 303);
+            this.btnWrite.Name = "btnWrite";
+            this.btnWrite.Size = new System.Drawing.Size(143, 31);
+            this.btnWrite.TabIndex = 0;
+            this.btnWrite.Text = "Write";
+            this.btnWrite.UseVisualStyleBackColor = true;
+            this.btnWrite.Click += new System.EventHandler(this.btnWrite_Click);
+            // 
+            // btnRread
+            // 
+            this.btnRread.Location = new System.Drawing.Point(275, 302);
+            this.btnRread.Name = "btnRread";
+            this.btnRread.Size = new System.Drawing.Size(103, 32);
+            this.btnRread.TabIndex = 1;
+            this.btnRread.Text = "Read";
+            this.btnRread.UseVisualStyleBackColor = true;
+            this.btnRread.Click += new System.EventHandler(this.btnRread_Click);
+            // 
+            // btnAppend
+            // 
+            this.btnAppend.Location = new System.Drawing.Point(64, 351);
+            this.btnAppend.Name = "btnAppend";
+            this.btnAppend.Size = new System.Drawing.Size(143, 32);
+            this.btnAppend.TabIndex = 2;
+            this.btnAppend.Text = "Append";
+            this.btnAppend.UseVisualStyleBackColor = true;
+            this.btnAppend.Click += new System.EventHandler(this.btnAppend_Click);
+            // 
+            // rtbWrite
+            // 
+            this.rtbWrite.Location = new System.Drawing.Point(64, 105);
+            this.rtbWrite.Name = "rtbWrite";
+            this.rtbWrite.Size = new System.Drawing.Size(143, 168);
+            this.rtbWrite.TabIndex = 3;
+            this.rtbWrite.Text = "";
+            // 
+            // rtbRead
+            // 
+            this.rtbRead.Location = new System.Drawing.Point(275, 105);
+            this.rtbRead.Name = "rtbRead";
+            this.rtbRead.Size = new System.Drawing.Size(143, 168);
+            this.rtbRead.TabIndex = 4;
+            this.rtbRead.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(471, 517);
+            this.Controls.Add(this.rtbRead);
+            this.Controls.Add(this.rtbWrite);
+            this.Controls.Add(this.btnAppend);
+            this.Controls.Add(this.btnRread);
+            this.Controls.Add(this.btnWrite);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnWrite;
+        private System.Windows.Forms.Button btnRread;
+        private System.Windows.Forms.Button btnAppend;
+        private System.Windows.Forms.RichTextBox rtbWrite;
+        private System.Windows.Forms.RichTextBox rtbRead;
     }
 }
 
--- FileHandlingExample/Form1.cs
+++ FileHandlingExample/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,5 +17,85 @@ namespace FileHandlingExample
         {
             InitializeComponent();
         }
+
+        private void btnWrite_Click(object sender, EventArgs e)
+        {
+            WriteToFile(false);
+        }
+
+        private void WriteToFile(bool append)
+        {
+            try
+            {
+                //This line will get the file path for the debug folder
+                string filePath = Application.StartupPath + @"\\data.txt";
+                //Pass the filepath and filename to the StreamWriter Constructor
+                StreamWriter sw = new StreamWriter(filePath, append);
+
+                //Write a line of text
+                sw.Write(rtbWrite.Text);
+                sw.Write("\n");
+
+                //Close the file
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+        }
+
+        private void btnAppend_Click(object sender, EventArgs e)
+        {
+            //Implement Append
+        }
+
+        private void btnRread_Click(object sender, EventArgs e)
+        {
+            rtbRead.Text = string.Empty;
+            ReadFile();
+        }
+
+        private void ReadFile()
+        {
+            string line;
+            try
+            {
+                //This line will get the file path for the debug folder
+                string filePath = Application.StartupPath + @"\\data.txt";
+
+                //Pass the file path and file name to the StreamReader constructor
+                StreamReader sr = new StreamReader(filePath);
+
+                //Read the first line of text
+                line = sr.ReadLine();
+
+                //Continue to read until you reach end of file
+                while (line != null)
+                {
+                    //write the line to console window
+                    rtbRead.Text += line;
+                    //Read the next line
+                    line = sr.ReadLine();
+                }
+
+                //close the file
+                sr.Close();
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+
+        }
     }
-}
+ }
